e8560a8d8a362553af4a7a10c5fce32f3abc166c
Ryan Farrow <rfarrow1010@gmail.com>
2025-01-03 12:43:12 -0800

    creating workflow

---
+++ Scripts/compile_json.py
@@ -0,0 +1,15 @@
+import json
+import glob
+
+# List all JSON files in the directory
+json_files = glob.glob("../Foods/*.json")
+combined_data = []
+
+for file in json_files:
+    with open(file, 'r') as f:
+        data = json.load(f)
+        combined_data.append(data)
+
+# Save the combined JSON file
+with open("../FoodData.json", "w") as outfile:
+    json.dump(combined_data, outfile, indent=4)
