3e6f4ae4540224dc9cbc6d75210d4cddc2e111db
Ryan Farrow <rfarrow1010@gmail.com>
2025-01-03 13:04:30 -0800

    clean-up

--- Scripts/compile_json.py
+++ Scripts/compile_json.py
@@ -4,15 +4,12 @@ import glob
 # List all JSON files in the directory
 json_files = glob.glob("Foods/*.json")
 combined_data = []
-print(json_files)
 
 for file in json_files:
     with open(file, 'r') as f:
-        print(f"Opening {file}")
         data = json.load(f)
         combined_data.append(data)
 
 # Save the combined JSON file
 with open("FoodData.json", "w") as outfile:
-    print("Writing file")
     json.dump(combined_data, outfile, indent=4)
